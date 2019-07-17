9b2115b25bc996ea47023db4d28f1d93376fd5c1
weechienUM <weechien@live.com>
2019-07-17 18:34:31 +0800

    commit

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '36dfdfb7754d4c9876f98f83b59f9200'
 
 posts = [
     {'title': 'Bloggie',
@@ -9,13 +12,35 @@ posts = [
     {'title': 'Joyful Platter',
      'author': 'Chng Yu Sing',
      'date': '15th July 2019',
-     'content': 'Come and join Joyful Platters!'}
+     'content': 'Come and join Joyful Platters!'},
+    {'title': 'Joyful Platter',
+     'author': 'Chng Yu Sing',
+     'date': '15th July 2019',
+     'content': 'Come and join Joyful Platters!'},
+    {'title': 'Joyful Platter',
+     'author': 'Chng Yu Sing',
+     'date': '15th July 2019',
+     'content': 'Come and join Joyful Platters!'},
+    {'title': 'Joyful Platter',
+     'author': 'Chng Yu Sing',
+     'date': '15th July 2019',
+     'content': 'Come and join Joyful Platters!'},
 ]
 
-@app.route("/")
+
+@app.route('/')
 def index():
     return render_template('index.html', posts=posts)
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
-if __name__ == "__main__":
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
     app.run(debug=True)
