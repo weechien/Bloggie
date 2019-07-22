1b52cbc32e9ac54766528bbced7591f217c4b7af
weechienUM <weechien@live.com>
2019-07-22 17:13:43 +0800

    commit

---
+++ application/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '36dfdfb7754d4c9876f98f83b59f9200'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
