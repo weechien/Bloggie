dd165cd647bae1987142dc4854abed6fbe6abf7b
weechien <weechien@live.com>
2019-07-24 00:36:55 +0800

    commit

--- application/config.py
+++ application/config.py
@@ -6,6 +6,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
-    MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
