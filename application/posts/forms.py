fb357a32b688ba643f2816661f56c27bd15cd247
weechien <weechien@live.com>
2019-07-24 23:53:50 +0800

    commit

--- application/posts/forms.py
+++ application/posts/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('P', 'Programming'), ('S', 'Science'), ('M', 'Machine Learning'), ('O', 'Other Tech')], default='O')
     submit = SubmitField('Post')
