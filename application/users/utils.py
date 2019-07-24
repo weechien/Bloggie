35df58601859e42c9ab8942aaa2c157e2e9a6238
weechien <weechien@live.com>
2019-07-24 21:39:08 +0800

    commit

--- application/users/utils.py
+++ application/users/utils.py
@@ -24,7 +24,7 @@ def save_picture(form_picture):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                  sender='gohweechien@gmail.com',
+                  sender=os.environ.get('EMAIL_USER'),
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
